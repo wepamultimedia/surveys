d4450bf02a1d9868ba4dd2c37fc6ab51eebef6f5
cpuche <cpuche@users.noreply.github.com>
2024-01-15 12:34:16 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix(config('blog.route_prefix', 'blog'))->middleware(['web', 'core.locale'])->group(function () {
-//    Route::get('/{start_at?}', [PostController::class, 'index'])->name('blog');
+    //    Route::get('/{start_at?}', [PostController::class, 'index'])->name('blog');
 });
